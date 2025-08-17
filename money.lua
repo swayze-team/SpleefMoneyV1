df5a9fd95cc0b50c55e73995a27caa1e5f94dae4
SwayzeFN-Modding <155722634+swayze-team@users.noreply.github.com>
2025-08-17 16:05:20 +0200

    Create money.lua

---
+++ money.lua
@@ -0,0 +1,17 @@
+-- Service
+local Event = game:GetService("ReplicatedStorage").Remotes.Server.CoinsUpdated
+
+-- Valeur de départ
+local value = 33227
+
+-- Boucle infinie
+while true do
+    -- On envoie l’événement avec la valeur courante
+    firesignal(Event.OnClientEvent, value, 10)
+
+    -- On ajoute +1000 pour la prochaine itération
+    value = value + 1000
+
+    -- Petite pause pour éviter de freeze (tu peux ajuster le temps)
+    task.wait(0.1)
+end
